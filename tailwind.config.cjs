c195331b08b9e42765e30626fd4f09e44e33556d
santiago villarreal arley <115122095+Villarley@users.noreply.github.com>
2024-09-08 20:30:13 -0600

    working

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
+  plugins: {
+    autoprefixer: {},
+  },
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
